a6d01eae3943cf7a38781001f8f76804e573a210
Marek <maro1977@o2.pl>
2020-02-09 15:19:26 +0100

    poprawa strony 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,17 @@
 import React from "react"
-
+import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1>NIE ZNALEZIONO STRONY</h1>
+    <h2>Prawdopodobnie masz zły adres, przejdz na stronę <Link to="/">HOME</Link>.</h2>
+
+
+
+
   </Layout>
 )
 
