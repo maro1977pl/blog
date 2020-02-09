d54aaa79f8b923e6cb79dd6b16d41ccb39e6ec17
Marek <maro1977@o2.pl>
2020-02-09 15:05:37 +0100

    Posprzątane

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
@@ -11,25 +10,7 @@ const IndexPage = () => (
 
     <h1>Cześć Wam testuje różności w Gatsby</h1>
     <p>To jest moja strona.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
 
-    <div>
-      <table>
-        <tr>
-          <td>1</td> <td>2</td> <td>3</td>
-        </tr>
-        <tr>
-          <td>4</td> <td>5</td> <td>6</td>
-        </tr>
-        <tr>
-          <td>7</td> <td>8</td> <td>9</td>
-        </tr>
-
-      </table>
-    </div>
     <Link to="/page-2/">Link do drugiej strony</Link>
   </Layout>
 )
